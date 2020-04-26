2d96d1705f2c39dddc61455872d68469299a3927
emelgunher <58791380+emelgunher@users.noreply.github.com>
2020-04-26 11:23:50 +0300

    Create Mail.cs

---
+++ Proje/Models/Mail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace Proje.Models
+{
+    public class Mail
+    {
+        public static void MailSender(string body)
+        {
+            var fromAddress = new MailAddress("emelgunher@gmail.com");
+            var toAddress = new MailAddress("emelgunher@gmail.com");
+            const string subject = "Site Adı | Sitenizden Gelen İletişim Formu";
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, "Trichohyalin.")
+            })
+            {
+                using (var message = new MailMessage(fromAddress, toAddress) { Subject = subject, Body = body })
+                {
+                    smtp.Send(message);
+                }
+            }
+        }
+    }
+}
